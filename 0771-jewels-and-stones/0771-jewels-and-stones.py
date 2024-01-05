9cf4b2974395f5672c09477a42072f37d8637fbe
KyungYoung Kim <87751531+KyungY-K@users.noreply.github.com>
2024-01-05 11:39:35 +0900

    Time: 35 ms (78.48%), Space: 17.4 MB (7.67%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        hash = collections.defaultdict(int)
+        count = 0
+        
+        for char in stones:
+            hash[char] += 1
+            
+        for char in jewels:
+            count += hash[char]
+            
+        return count
+            
